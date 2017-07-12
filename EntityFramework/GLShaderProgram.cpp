92870d06807d7641632b30068686ad625067d75b
Kuba Maruszczyk <kuba_maruszczyk@hotmail.co.uk>
2017-07-12 19:52:33 +0100

    Added GLShaderProgram implementation

--- EntityFramework/GLShaderProgram.cpp
+++ EntityFramework/GLShaderProgram.cpp
@@ -1,12 +1,39 @@
 #include "GLShaderProgram.h"
 
 
-
-GLShaderProgram::GLShaderProgram()
+namespace KLM_FRAMEWORK
 {
-}
+	GLShaderProgram::GLShaderProgram(GLShader * vertexShader, GLShader * fragmentShader):
+		p_VertexShader{vertexShader},
+		p_FragmentShader{fragmentShader}
+	{
+	}
 
+	GLShaderProgram::~GLShaderProgram()
+	{
+		if (p_VertexShader) glDetachShader(m_ProgId, p_VertexShader->GetID());
+		if (p_FragmentShader) glDetachShader(m_ProgId, p_FragmentShader->GetID());
+		glDeleteProgram(m_ProgId);
+	}
 
-GLShaderProgram::~GLShaderProgram()
-{
+	bool GLShaderProgram::Create()
+	{
+		if (m_Created) return true;
+		m_ProgId = glCreateProgram();
+		glAttachShader(m_ProgId, p_VertexShader->GetID());
+		glAttachShader(m_ProgId, p_FragmentShader->GetID());
+		glLinkProgram(m_ProgId);
+		m_Created = true;
+		return true;
+	}
+
+	bool GLShaderProgram::SetAsCurrent()
+	{
+		if (p_VertexShader&&p_FragmentShader)
+		{
+			glUseProgram(m_ProgId);
+			return true;
+		}
+		return false;
+	}
 }
--- EntityFramework/GLShaderProgram.h
+++ EntityFramework/GLShaderProgram.h
@@ -1,8 +1,27 @@
 #pragma once
-class GLShaderProgram
+#include "GLShader.h"
+namespace KLM_FRAMEWORK
 {
-public:
-	GLShaderProgram();
-	virtual ~GLShaderProgram();
-};
+	class GLShaderProgram
+	{
 
+	private:
+		GLuint m_ProgId;
+		GLShader* p_VertexShader;
+		GLShader* p_FragmentShader;
+		bool m_Created{ false };
+
+
+	public:
+		GLShaderProgram(GLShader* vertexShader, GLShader* fragmentShader);
+		virtual ~GLShaderProgram();
+
+		GLShaderProgram& operator=(GLShaderProgram&) = delete;
+		GLShaderProgram(const GLShaderProgram&) = delete;
+
+		bool Create();
+		bool SetAsCurrent();
+		GLuint GetID() { return m_ProgId; }
+	};
+
+}
