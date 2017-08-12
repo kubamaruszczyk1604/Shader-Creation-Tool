af439d3ca12709d97ca3a5595cfae7312c9ccf86
Kuba Maruszczyk <kuba_maruszczyk@hotmail.co.uk>
2017-08-12 22:57:42 +0100

    residual update

--- ShaderCreationTool/Forms/SCTConsole.Designer.cs
+++ ShaderCreationTool/Forms/SCTConsole.Designer.cs
@@ -56,6 +56,7 @@
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(824, 481);
             this.Controls.Add(this.ConsoleTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "SCTConsole";
