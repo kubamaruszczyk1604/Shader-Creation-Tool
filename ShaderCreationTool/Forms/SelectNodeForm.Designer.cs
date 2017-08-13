7f02cc7409a22d1d566d83fc7a3c1ac3e9a9da0f
Kuba Maruszczyk <kuba_maruszczyk@hotmail.co.uk>
2017-08-14 00:44:14 +0100

    residual update

--- ShaderCreationTool/Forms/SelectNodeForm.Designer.cs
+++ ShaderCreationTool/Forms/SelectNodeForm.Designer.cs
@@ -36,13 +36,14 @@
             // listBox1
             // 
             this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.ForeColor = System.Drawing.Color.Lime;
+            this.listBox1.ForeColor = System.Drawing.Color.Gray;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
             this.listBox1.Location = new System.Drawing.Point(0, 28);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(376, 276);
+            this.listBox1.Size = new System.Drawing.Size(328, 228);
             this.listBox1.TabIndex = 0;
             this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
             this.listBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.listBox1_KeyPress);
@@ -53,11 +54,11 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(14, 9);
+            this.label1.Location = new System.Drawing.Point(61, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(348, 16);
+            this.label1.Size = new System.Drawing.Size(194, 16);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Select functionality node you would like to create.";
+            this.label1.Text = "Select node type to create.";
             // 
             // SelectNodeForm
             // 
@@ -65,7 +66,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(374, 305);
+            this.ClientSize = new System.Drawing.Size(325, 255);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.ForeColor = System.Drawing.Color.Black;
