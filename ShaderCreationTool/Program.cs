3506f15355d6ce0b21d38c2075cea8f5bce35690
Kuba Maruszczyk <kuba_maruszczyk@hotmail.co.uk>
2017-08-14 13:47:42 +0100

    residual update

--- ShaderCreationTool/Program.cs
+++ ShaderCreationTool/Program.cs
@@ -14,7 +14,6 @@ namespace ShaderCreationTool
         [STAThread]
         static void Main()
         {
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
