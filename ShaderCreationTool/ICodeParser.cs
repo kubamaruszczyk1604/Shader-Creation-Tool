1ff114f200f789ee06a70ddf8e58251098fe2c5d
Kuba Maruszczyk <kuba_maruszczyk@hotmail.co.uk>
2017-08-17 12:00:39 +0100

    Parser Interface created

---
+++ ShaderCreationTool/ICodeParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShaderCreationTool
+{
+    interface ICodeParser
+    {
+        bool TranslateInputVariables(List<IInputNode> inputNodes, out string declarationsCode,out string status);
+        bool TranslateNodeIntoFunction(SCTFunctionNode node, out string functionCode, out string status);
+        bool TranslateNetwork(List<ISCTNode> nodes, List<Connection> connections, out string code, out string status);
+    }
+}
